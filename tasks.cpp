21732e2a9ef0ea5fd263b61966482b4dc1e06ea7
Aryan Atif <aryanatif01@gmail.com>
2026-01-14 11:12:35 +0500

    Check the existence of the config file

--- tasks.cpp
+++ tasks.cpp
@@ -9,11 +9,12 @@
 void open_config_file();
 void open_config_file(std::string);
 
+void read_file(std::fstream file);
 
 int main(int argc, char* argv[])
 {
+  //for debugging
   std::cout << "argc = " << argc << std::endl;
-
   std::string command[argc - 1];
 
   for (int i = 1; i < argc; i++)
@@ -33,6 +34,7 @@ int main(int argc, char* argv[])
   else if (argc <= 3 && (command[1] == "-a" || command[1] == "-l" || command[1] == "-d"))               // > ./task -l || > ./task -l <something>
   {
     open_config_file();
+
   }
   /* else if (argc <= 3 && (argv[1] == "--add" ||argv[1] == "--list" || argv[1] == "--delete")) //>./task --list || >./task --list<something>
   {
@@ -40,8 +42,7 @@ int main(int argc, char* argv[])
   } */
   else
   {
-    std::cout << "in else" << std::endl;
-    std::cout << "Syntax: ./task --command <task>" << std::endl; 
+    std::cout << "in else" << std::endl; std::cout << "Syntax: ./task --command <task>" << std::endl; 
   }
 }
 
@@ -50,21 +51,31 @@ void open_config_file()
 {
   const char* home = std::getenv("HOME");
 
-  if (!home) {std::cerr << "There was an error finding the home directory" << std::endl; return;} // add exceptions here later
-
+  if (!home) {std::cerr << "There was an error finding the home directory" << std::endl; return;} // TODO: add exceptions
+                                                                                                  
   // create an object of the class std::filesystem::path and assign the path to the home directory to it.
   std::filesystem::path config_file_path = home;     
 
-  config_file_path /= ".tasks";
+  config_file_path /= ".task.conf";
 
-  std::ofstream config_file(config_file_path);
+  std::fstream config_file(config_file_path, std::ios::in | std::ios::out | std::ios::app);
   
-  if(!config_file)
-  {
-    std::cerr << "Error opening the config file" << std::endl;
-  }
-  else
-  {
-    std::cout << "File opened!" << std::endl;
-  }
+  if(!config_file) { std::cerr << "Error opening the config file" << std::endl; return;} // TODO: Add exceptions
+  
+  if(config_file.gcount() == 0) { std::cerr << "The config file is empty. Run ./tasks -c or ./tasks --config to create it." << std::endl; return;}
+  
+  // For debugging
+  std::cout << "File opened!" << std::endl;
+
+  //TODO: Read the config file to get the path for the md file.
 }
+
+
+void read_file(std::fstream file)
+{
+  
+  //for (int i = 0; ; i++)
+   
+}
+
+
