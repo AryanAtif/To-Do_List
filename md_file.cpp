6cc24852d2eb90b50efae629d1195f7dbc0e0b8a
Aryan Atif <aryanatif01@gmail.com>
2026-01-23 10:29:07 +0500

    Fix Path validity check

--- md_file.cpp
+++ md_file.cpp
@@ -2,17 +2,23 @@
 #include <fstream>
 #include <filesystem>
 
+#include "md_file.h"
 
 // open the markdown file 
 void open_md_file(std::string& path)
 {
-  bool is_valid_path (std::string &path);
+  if (is_valid_path(path)) { std::cout << "The path is valid" << std::endl; }
+
+  else { std::cout << "The path is invalid" << std::endl;}
 
   // Erase the double quotes and the " " and "~" characters from the address.
   path.erase (0,2); // Erase '"~' from the beginning
   path.erase(path.length()-1, path.length());  //  erase the last '"' 
 
   std::cout << "the path after the ops: " << path << std::endl;
+
+  if (is_valid_path(path)) { std::cout << "The path is valid" << std::endl; }
+  else { std::cout << "The path is invalid" << std::endl;}
 /*
   const char* home = std::getenv("HOME");
   if (!home) {std::cerr << "There was an error finding the home directory." << std::endl; return;}
@@ -25,9 +31,9 @@ void open_md_file(std::string& path)
 
 bool is_valid_path (std::string &path)
 {
-  char file_format[4];
-  std::size_t length = path.copy(file_format, 3, path.length()-4);
-
-  if (file_format = ".md") {return 1;}
+  // check if the sequence ".md" makes up the last three characters of the string
+  size_t path_found = path.rfind(".md");
+  int file_extension_size = 3; 
+  if (path_found != path.npos && (path.size() - path_found == file_extension_size)) {return 1;} 
   return 0;
-}
+} 
